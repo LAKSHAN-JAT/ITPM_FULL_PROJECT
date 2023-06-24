319adfe02f6bf5e87b1eeaaf086282159e402359
LAKSHAN J.A.T <91153687+LAKSHAN-JAT@users.noreply.github.com>
2023-06-24 23:14:39 +0530

    deleted

--- ITPM_PROJECT/resources/views/layout/cmenu.blade.php
+++
@@ -1,5 +0,0 @@
-<li class="menu-header">Main Menu</li>
-<li><a class="nav-link" href="{{url('/cdashboard')}}"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
-<li class="menu-header">Customer Section</li>
-<li><a class="nav-link" href="{{url('requirements')}}"><i class="fas fa-table"></i> <span>Requirements Manage</span></a></li>
-
